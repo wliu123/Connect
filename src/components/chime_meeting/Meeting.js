8b9fa7af080b6036116132616c12734e38a3c668
William Liu <williamliu626@gmail.com>
2022-05-25 20:37:15 -0400

    attempting to add videochat

---
+++ src/components/chime_meeting/Meeting.js
@@ -0,0 +1,21 @@
+// import { FC } from "react";
+// import {
+//     AudioInputControl,
+//     AudioOutputControl,
+//     ControlBar,
+//     ControlBarButton,
+//     Phone,
+//     useMeetingManager,
+//     MeetingStatus,
+//     useMeetingStatus,
+//     VideoInputControl,
+//     VideoTileGrid
+// } from 'amazon-chime-sdk-component-library-react'
+// import {endMeeting}
+
+// const Meeting = () => {
+    
+//     return ()
+    
+// }
+// export default Meeting
