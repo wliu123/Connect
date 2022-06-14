739c86967381baf1665aedee1703be1b9c571062
William Liu <williamliu626@gmail.com>
2022-06-14 14:55:54 -0700

    updated auth from amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,6 @@ export type AmplifyDependentResourcesAttributes = {
         "connect": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
